64677fdcfadeeeab007f2f87c2876dfb87c8f820
Apple <meetseanthomas@gmail.com>
2020-04-06 21:23:16 -0700

    Adding toasts and upgrading contact form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,9 @@ import { NavbarComponent } from './navbar/navbar.component';
 import { CaresActComponent } from './cares-act/cares-act.component';
 import { SbaLoanWorkflowComponent } from './sba-loan-workflow/sba-loan-workflow.component';
 import { SitemapComponent } from './sitemap/sitemap.component';
+import { ToastsComponent } from './shared/toasts/toasts.component';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import {FormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -16,11 +19,14 @@ import { SitemapComponent } from './sitemap/sitemap.component';
     NavbarComponent,
     CaresActComponent,
     SbaLoanWorkflowComponent,
-    SitemapComponent
+    SitemapComponent,
+    ToastsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [
--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,35 +1,29 @@
 import { Component, OnInit } from '@angular/core';
 import emailjs, { EmailJSResponseStatus } from 'emailjs-com';
 import * as $ from 'jquery';
+import {ToastsService} from '../shared/toasts/toasts.service';
 
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
   styleUrls: ['./contact.component.scss'],
 })
-export class ContactComponent implements OnInit {
+export class ContactComponent {
 
-  constructor() {}
+  constructor(private toastService: ToastsService) {}
   private emailServiceId = 'cobizsupp@gmail.com';
   private emailTemplateId = 'template_tStaUmvL';
   private emailUserId = 'user_D20DRjAm3VTglgEt4Fcqa';
 
-  ngOnInit(): void {
-    $(document).ready(() => {
-      $('form').submit(() => {
-        alert('Your message was recieved. Thanks for getting in contact!');
-        location.reload();
-      });
-    });
-  }
-
-  public sendEmail(e: Event) {
+  public sendEmail(e: Event, contactForm: any) {
     e.preventDefault();
     emailjs.sendForm(this.emailServiceId, this.emailTemplateId, e.target as HTMLFormElement, this.emailUserId)
       .then((result: EmailJSResponseStatus) => {
-        console.log(result.text);
+        this.toastService.show('Success', 'Your message was received. Thanks for getting in contact!');
+        contactForm.reset();
       }, (error) => {
         console.log(error.text);
+        this.toastService.show('Error', 'Well this is embarrassing, an error happened. ');
       });
   }
 }
